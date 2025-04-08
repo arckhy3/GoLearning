dab8bc2719065c62d40ddf0397a8c0d2f94f66cb
arckhy <74829523+arckhy3@users.noreply.github.com>
2025-04-08 16:24:22 +0700

    create api get all and create event also add sqlite

---
+++ 12/models/event.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"time"
+
+	"example.com/event/db"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int64
+}
+
+func (e Event) Save() (Event, error) {
+	query := `
+	INSERT INTO events(name,description,location,datetime,user_id)
+	VALUES(?,?,?,?,?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return e, err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return e, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return e, err
+	}
+	e.ID = id
+	return e, err
+}
+
+func GetAll() ([]Event, error) {
+	var events = []Event{}
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return events, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var event = Event{}
+		err = rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, err
+}
