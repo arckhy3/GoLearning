cb7bfa0364f9ecb548d9c31960d2c755e6c6f9bb
arckhy <74829523+arckhy3@users.noreply.github.com>
2025-06-02 21:50:45 +0700

    update code

--- 12/Routes/user.go
+++ 12/Routes/user.go
@@ -2,41 +2,41 @@ package routes
 
 import (
 	"net/http"
-	"strconv"
+	// "strconv"
 
 	"example.com/event/models"
 	"example.com/event/utils"
 	"github.com/gin-gonic/gin"
 )
 
-func getUsers(context *gin.Context) {
-	users, err := models.GetAllUser()
+// func getUsers(context *gin.Context) {
+// 	users, err := models.GetAllUser()
 
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to get user", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to get user", "error": err.Error()})
+// 		return
+// 	}
 
-	context.JSON(http.StatusOK, users)
-}
+// 	context.JSON(http.StatusOK, users)
+// }
 
-func getUser(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+// func getUser(context *gin.Context) {
+// 	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
+// 		return
+// 	}
 
-	user, err := models.FindUserByID(id)
+// 	user, err := models.FindUserByID(id)
 
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to get user", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to get user", "error": err.Error()})
+// 		return
+// 	}
 
-	context.JSON(http.StatusOK, user)
-}
+// 	context.JSON(http.StatusOK, user)
+// }
 
 func saveUser(context *gin.Context) {
 	var user = models.User{}
@@ -57,59 +57,59 @@ func saveUser(context *gin.Context) {
 	context.JSON(http.StatusCreated, user)
 }
 
-func updateUser(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+// func updateUser(context *gin.Context) {
+// 	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
+// 		return
+// 	}
 
-	var updatedUser = models.Event{}
-	err = context.ShouldBind(&updatedUser)
+// 	var updatedUser = models.Event{}
+// 	err = context.ShouldBind(&updatedUser)
 
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
+// 		return
+// 	}
 
-	updatedUser.ID = id
+// 	updatedUser.ID = id
 
-	err = updatedUser.UpdateByID()
+// 	err = updatedUser.UpdateByID()
 
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create user", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create user", "error": err.Error()})
+// 		return
+// 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "User update succes", "user": updatedUser})
-}
+// 	context.JSON(http.StatusOK, gin.H{"message": "User update succes", "user": updatedUser})
+// }
 
-func deleteUser(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+// func deleteUser(context *gin.Context) {
+// 	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err.Error()})
+// 		return
+// 	}
 
-	user, err := models.FindUserByID(id)
+// 	user, err := models.FindUserByID(id)
 
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to delete user", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to delete user", "error": err.Error()})
+// 		return
+// 	}
 
-	err = user.Delete()
+// 	err = user.Delete()
 
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to delete user", "error": err.Error()})
-		return
-	}
+// 	if err != nil {
+// 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to delete user", "error": err.Error()})
+// 		return
+// 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "User delete success"})
+// 	context.JSON(http.StatusOK, gin.H{"message": "User delete success"})
 
-}
+// }
 
 func login(context *gin.Context) {
 	var user = models.User{}
